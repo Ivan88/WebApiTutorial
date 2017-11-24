b6b879fa04c6505a00f9144d0b4efc50fd6c40f0
Ivan Omelchenko <omelivan@mail.ru>
2017-11-24 10:02:10 +0200

    added new web forms project

---
+++ WebFormsExample/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebFormsExample
+{
+	public class Global : HttpApplication
+	{
+		void Application_Start(object sender, EventArgs e)
+		{
+			RouteTable.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional });
+
+			// Code that runs on application startup
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+		}
+	}
+}
